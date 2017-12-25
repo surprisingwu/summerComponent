69cbed6a4342669025a58c6e690d99798afa9ca4
surprisingwu <surprise.wu@foxmail.com>
2017-12-25 08:24:57 +0800

    完成base navigator的开发

--- js/components/index.js
+++ js/components/index.js
@@ -23,18 +23,24 @@ components.navigator.NavigatorBase = {
         '<div class="um-header-light">' +
         '<div v-show="isBack"><a href="#" class="um-back" @click="goToBack" v-show="isShowIconBack"><span v-show="isShowIconText">返回</span></a></div>' +
         '<h3> 导航栏</h3>' +
-        '<div><div class="um-header-btns" v-show="isShowBtnIcon"><i class="ti-plus mr5" ></i></div><a class="um-header-right" href="#" v-show="isShowBtnText">保存</a></div>' +
+        '<div v-show="isShowRight"><div class="um-header-btns" v-show="isShowBtnIcon"><i class="ti-plus mr5" ></i></div><a class="um-header-right" href="#" v-show="isShowBtnText">保存</a></div>' +
         '</div>' +
         '<div class="um-content">' +
         '<div class="text-content">这是导航栏(顶部)的默认的演示样式,你可以在顶部导航中防止多种控件。点击如下单选框按钮切换导航栏的内容</div>' +
         '<div class="components-title">左侧显示内容</div>' +
         '<div class="um-check-group">' +
-        '<label class="um-check-group-item border-1px" v-for="(item,index) in radioMesg">' +
-        '<input name="um-radio" type="radio" :checked="index === 1"  :value="item.value" v-model="preValueLeft">' +
+        '<label class="um-check-group-item border-1px" v-for="item in radioMesg">' +
+        '<input name="um-radio" type="radio" :checked="item.text === preValueLeft" :value="item.value" v-model="preValueLeft">' +
         '<span class="um-icon-check um-css3-vc"></span>' +
         '<span class="um-black">{{item.text}}</span>' +
         '</label></div>' +
         '<div class="components-title">右侧显示内容</div>' +
+        '<div class="um-check-group">' +
+        '<label class="um-check-group-item border-1px" v-for="item in radioMesgRight">' +
+        '<input name="um-radio2" type="radio" :checked="item.text === preValueRight" :value="item.value" v-model="preValueRight">' +
+        '<span class="um-icon-check um-css3-vc"></span>' +
+        '<span class="um-black">{{item.text}}</span>' +
+        '</label></div>' +
         '</div>' +
         '</div>',
     data: function() {
@@ -42,25 +48,31 @@ components.navigator.NavigatorBase = {
             isBack: true,
             isShowIconBack: true,
             isShowIconText: false,
+            isShowRight: false,
             isShowBtnText: false,
             isShowBtnIcon: false,
-            preValueLeft: '',
+            preValueLeft: 'isShowIconBack',
+            preValueRight: 'isShowRight'
         }
     },
     mixins: [mixins],
-    created: function() {
-        this.preValueLeft = ''
-        this.preValueRight = ''
-    },
+    created: function() {},
     computed: {
         radioMesg: function() {
             var radioMesg = [
-                { text: '不显示', value: 'isBack', bol: this.isBack },
-                { text: '仅图标', value: 'isShowIconBack', bol: this.isShowIconBack },
-                { text: '图标加文字', value: 'isShowIconText', bol: this.isShowIconText }
+                { text: '不显示', value: 'isBack' },
+                { text: '仅图标', value: 'isShowIconBack' },
+                { text: '图标加文字', value: 'isShowIconText' }
             ]
-
             return radioMesg
+        },
+        radioMesgRight: function() {
+            var ret = [
+                { text: '不显示', value: 'isShowRight' },
+                { text: '仅图标', value: 'isShowBtnIcon' },
+                { text: '文字按钮', value: 'isShowBtnText' }
+            ]
+            return ret
         }
     },
     methods: {},
@@ -80,6 +92,24 @@ components.navigator.NavigatorBase = {
                     this.isBack = false
                 } else {
                     this.isBack = true
+                }
+            }
+        },
+        preValueRight: function(newVal, oldVal) {
+            if (newVal !== oldVal) {
+
+                if (newVal === 'isShowBtnText') {
+                    this.isShowBtnIcon = false
+                    this.isShowBtnText = true
+                }
+                if (newVal === 'isShowBtnIcon') {
+                    this.isShowBtnText = false
+                    this.isShowBtnIcon = true
+                }
+                if (newVal === 'isShowRight') {
+                    this.isShowRight = false
+                } else {
+                    this.isShowRight = true
                 }
             }
         }
